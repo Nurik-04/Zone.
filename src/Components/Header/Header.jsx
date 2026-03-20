f6f6258493c09521d4f961e2411e4b1b20cfcd6f
Nurik-04 <alpisbayevnurdaulet@gmail.com>
2026-03-20 23:36:18 +0500

    i18n.js fayili korib chiqilib hatolari tog'rilandi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,45 +1,74 @@
-import logo from '../Images/logo.svg'
-import React from 'react'
-// import logo0 from '../Images/logo0.svg';
-import { useTranslation } from 'react-i18next';
-import "./header.css"
+import logo from "../Images/logo.svg";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./header.css";
 
 const Header = () => {
-    const [t, i18n] = useTranslation();
-    const handleChangeCity = (event) => {
-        const selectedLanguage = event.target.value;
-        i18n.changeLanguage(selectedLanguage);
-    };
+  const [t, i18n] = useTranslation();
+  const handleChangeCity = (event) => {
+    const selectedLanguage = event.target.value;
+    i18n.changeLanguage(selectedLanguage);
+  };
   return (
     <header>
-        <div className="container">
-            <nav className='header__nav'>
-                <a className='header__title'>
-                    <img className='header__img' src={logo} alt="" />
-                </a>
-                <ul className='header__list'>
-                    <li className='header__item'><a href="#" className='header__link'>{t("header.home")}</a></li>
-                    <li className='header__item'><a href="#" className='header__link'>{t("header.about")}</a></li>
-                    <li className='header__item'><a href="#" className='header__link'>{t("header.service")}</a></li>
-                    <li className='header__item'><a href="#" className='header__link'>{t("header.pricing")}</a></li>
-                    <li className='header__item'><a href="#" className='header__link'>{t("header.blog")}</a></li>
-                    <li className='header__item'><a href="#" className='header__btn__link'>{t("header.contact")}</a></li>
-                    <li className='header__item'>
-                        <select 
-                            className="buttons" 
-                            onChange={handleChangeCity} 
-                            value={i18n.language} // Hozirgi tanlangan tilni ko'rsatib turadi
-                                >
-                            <option className='lenguage' value="en">EN</option>
-                            <option className='lenguage' value="ru">RU</option>
-                            <option className='lenguage' value="uz">UZ</option>
-                        </select>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+      <div className="container">
+        <nav className="header__nav">
+          <a className="header__title">
+            <img className="header__img" src={logo} alt="" />
+          </a>
+          <ul className="header__list">
+            <li className="header__item">
+              <a href="#" className="header__link">
+                {t("header.home")}
+              </a>
+            </li>
+            <li className="header__item">
+              <a href="#" className="header__link">
+                {t("header.about")}
+              </a>
+            </li>
+            <li className="header__item">
+              <a href="#" className="header__link">
+                {t("header.service")}
+              </a>
+            </li>
+            <li className="header__item">
+              <a href="#" className="header__link">
+                {t("header.pricing")}
+              </a>
+            </li>
+            <li className="header__item">
+              <a href="#" className="header__link">
+                {t("header.blog")}
+              </a>
+            </li>
+            <li className="header__item">
+              <a href="#" className="header__btn__link">
+                {t("header.contact")}
+              </a>
+            </li>
+            <li className="header__item">
+              <select
+                className="buttons"
+                onChange={handleChangeCity}
+                value={i18n.language} // Hozirgi tanlangan tilni ko'rsatib turadi
+              >
+                <option className="lenguage" value="en">
+                  EN
+                </option>
+                <option className="lenguage" value="ru">
+                  RU
+                </option>
+                <option className="lenguage" value="uz">
+                  UZ
+                </option>
+              </select>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/i18n.js
+++ src/i18n.js
@@ -6,19 +6,43 @@ import enT from "./Locals/en.json";
 
 i18n
 
-.use(initReactI18next)
-.init({
-    resources:{
-        uz:{translation: uzT},
-        ru:{translation: ruT},
-        en:{translation: enT},
-    },
-    len: "en",
-    fallbackLng: "uz",
-    debug: true,
-    interpolition:{
-        escapeValue: false,
-    },
-})
+    .use(initReactI18next)
+    .init({
+        resources: {
+            uz: { translation: uzT },
+            ru: { translation: ruT },
+            en: { translation: enT },
+        },
+        lng: "en",
+        fallbackLng: "uz",
+        debug: true,
+        interpolation: {
+            escapeValue: false,
+        },
+    })
 
 export default i18n
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// // Papka nomini aniq tekshiring: Locals yoki locales?
+// import uzT from "./Locals/uz.json"; 
+// import ruT from "./Locals/ru.json";
+// import enT from "./Locals/en.json";
+
+// i18n
+//   .use(initReactI18next)
+//   .init({
+//     resources: {
+//       uz: { translation: uzT },
+//       ru: { translation: ruT },
+//       en: { translation: enT },
+//     },
+//     lng: "en", // 'len' emas, 'lng' bo'lishi shart
+//     fallbackLng: "uz",
+//     debug: true,
+//     interpolation: { // 'interpolition' emas, 'interpolation'
+//       escapeValue: false,
+//     },
+//   });
+
+// export default i18n;
