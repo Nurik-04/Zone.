582b60a842324af08de67de05bb09439555fc26b
Nurik-04 <alpisbayevnurdaulet@gmail.com>
2026-03-20 23:41:16 +0500

    Lccals papkasini nomi locales'ga ozgartirildi xatoni tog;rilash maqsadida

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import uzT from "./Locals/uz.json";
-import ruT from "./Locals/ru.json";
-import enT from "./Locals/en.json";
+import uzT from "./locales/uz.json";
+import ruT from "./locales/ru.json";
+import enT from "./locales/en.json";
 
 i18n
 
@@ -22,27 +22,3 @@ i18n
     })
 
 export default i18n
-// import i18n from "i18next";
-// import { initReactI18next } from "react-i18next";
-// // Papka nomini aniq tekshiring: Locals yoki locales?
-// import uzT from "./Locals/uz.json"; 
-// import ruT from "./Locals/ru.json";
-// import enT from "./Locals/en.json";
-
-// i18n
-//   .use(initReactI18next)
-//   .init({
-//     resources: {
-//       uz: { translation: uzT },
-//       ru: { translation: ruT },
-//       en: { translation: enT },
-//     },
-//     lng: "en", // 'len' emas, 'lng' bo'lishi shart
-//     fallbackLng: "uz",
-//     debug: true,
-//     interpolation: { // 'interpolition' emas, 'interpolation'
-//       escapeValue: false,
-//     },
-//   });
-
-// export default i18n;
